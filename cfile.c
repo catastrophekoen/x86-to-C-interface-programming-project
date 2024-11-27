57693fc16c42d6eecbaea95f5ad67cf447da6443
Jeremy Koen K. Palacios <palacios22.koenk@gmail.com>
2024-11-27 08:31:17 +0800

    cleaned up code

--- cfile.c
+++ cfile.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Assembly function prototype
 extern void compute_accelerations(int num_cars, double* input_matrix, int* output_results);
 
 int main() {
     int num_cars;
     int i;
+    
     //printf("Enter the number of cars: ");
     scanf("%d", &num_cars);
 
@@ -19,14 +19,12 @@ int main() {
         return 1;
     }
 
-    // Input data for each car
     //printf("Enter the matrix values (Initial Velocity, Final Velocity, Time):\n");
     for (i = 0; i < num_cars; i++) {
         //printf("Car %d: ", i + 1);
         scanf("%lf,%lf,%lf", &input_matrix[i * 3], &input_matrix[i * 3 + 1], &input_matrix[i * 3 + 2]);
     }
 
-    // Call the assembly function
     compute_accelerations(num_cars, input_matrix, output_results);
 
     // Display results
